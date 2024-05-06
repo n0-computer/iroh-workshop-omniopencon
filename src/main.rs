559409e90ce3c171ff8fee4156373ebad2008860
Ruediger Klaehn <rklaehn@protonmail.com>
2024-05-06 17:25:53 +0200

    Start working on accept

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ const PIPE_ALPN: &[u8] = b"JOTB_PIPE";
 #[derive(Debug, clap::Parser)]
 struct Args {
     /// Ticket to connect to. If not provided, the program will listen for incoming connections.
-    ticket: NodeTicket,
+    ticket: Option<NodeTicket>,
 }
 
 /// Connect to a remote node using a ticket.
@@ -38,13 +38,37 @@ async fn connect(ticket: NodeTicket) -> anyhow::Result<()> {
     Ok(())
 }
 
+/// Accept incoming connections.
+async fn accept() -> anyhow::Result<()> {
+    let secret_key = SecretKey::generate();
+    let public_key = secret_key.public();
+    let endpoint = MagicEndpoint::builder()
+        .secret_key(secret_key)
+        .alpns(vec![PIPE_ALPN.to_vec()])
+        .bind(0)
+        .await?;
+    let addr = endpoint.my_addr().await?;
+    println!("I am {}", addr.node_id);
+    println!("Listening on {:#?}", addr.info);
+    println!("ticket: {}", NodeTicket::new(addr)?);
+    while let Some(connecting) = endpoint.accept().await {
+        // handle each incoming connection in separate tasks.
+        todo!();
+    }
+    Ok(())
+}
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     // init logging. we can now configure the log level with the RUST_LOG environment variable.
     tracing_subscriber::fmt::init();
     // Parse the command line arguments.
     let args = Args::parse();
-    // Code goes here
-    connect(args.ticket).await?;
+    // if a ticket is provided, connect to the remote node, otherwise accept incoming connections.
+    if let Some(ticket) = args.ticket {
+        connect(ticket).await?;
+    } else {
+        accept().await?;
+    }
     Ok(())
 }
