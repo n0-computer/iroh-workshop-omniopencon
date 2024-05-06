561d76bffc8b8494401238dc01d206281190af45
Ruediger Klaehn <rklaehn@protonmail.com>
2024-05-06 16:03:11 +0200

    Project setup

---
+++ src/main.rs
@@ -0,0 +1,7 @@
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    // init logging. we can now configure the log level with the RUST_LOG environment variable.
+    tracing_subscriber::fmt::init();
+    // Code goes here
+    Ok(())
+}
