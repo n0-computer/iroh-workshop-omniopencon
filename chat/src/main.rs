aa94c9be8bd9d20fe31945d52142cb7e7a6fac19
Ruediger Klaehn <rklaehn@protonmail.com>
2024-05-06 18:56:32 +0200

    Add command line args

--- chat/src/main.rs
+++ chat/src/main.rs
@@ -1,5 +1,15 @@
+use clap::Parser;
+use iroh_net::ticket::NodeTicket;
+
+#[derive(Debug, Parser)]
+struct Args {
+    tickets: Vec<NodeTicket>,
+}
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    tracing_subscriber::fmt::init();
+    let args = Args::parse();
     // chat goes here
     Ok(())
 }
