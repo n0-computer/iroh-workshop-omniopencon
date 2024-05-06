2ba7dda9b25af15c39ac267b55f474bbcc80cace
Ruediger Klaehn <rklaehn@protonmail.com>
2024-05-06 19:02:09 +0200

    Create the endpoint and copy the utils from the pipe example

--- chat/src/main.rs
+++ chat/src/main.rs
@@ -1,5 +1,10 @@
 use clap::Parser;
-use iroh_net::ticket::NodeTicket;
+use iroh_base::node_addr::AddrInfoOptions;
+use iroh_gossip::proto::TopicId;
+use iroh_net::{ticket::NodeTicket, MagicEndpoint};
+
+mod util;
+use util::*;
 
 #[derive(Debug, Parser)]
 struct Args {
@@ -10,6 +15,23 @@ struct Args {
 async fn main() -> anyhow::Result<()> {
     tracing_subscriber::fmt::init();
     let args = Args::parse();
+    let secret_key = get_or_create_secret()?;
+    let public_key = secret_key.public();
+    let topic = TopicId::from([0u8; 32]);
+
+    let endpoint = MagicEndpoint::builder()
+        .secret_key(secret_key.clone())
+        .alpns(vec![iroh_gossip::net::GOSSIP_ALPN.to_vec()])
+        .bind(0)
+        .await?;
+    let mut my_addr = endpoint.my_addr().await?;
+    let ticket = NodeTicket::new(my_addr.clone())?;
+    println!("I am {}", my_addr.node_id);
+    println!("Connect to me using {}", ticket);
+    my_addr.apply_options(AddrInfoOptions::Id);
+    let short = NodeTicket::new(my_addr.clone())?;
+    println!("Connect to me using {}", short);
+    wait_for_relay(&endpoint).await?;
     // chat goes here
     Ok(())
 }
