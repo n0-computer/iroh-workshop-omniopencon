f767d2867827fe744e255889ffa4baf2b94bd84d
Ruediger Klaehn <rklaehn@protonmail.com>
2024-05-07 22:24:10 +0200

    All versions of pipe and chat as full, separate rust programs

---
+++ chat/version1/src/main.rs
@@ -0,0 +1,104 @@
+use clap::Parser;
+use iroh_base::node_addr::AddrInfoOptions;
+use iroh_gossip::{
+    net::{Event, Gossip},
+    proto::TopicId,
+};
+use iroh_net::{
+    discovery::{dns::DnsDiscovery, pkarr_publish::PkarrPublisher, ConcurrentDiscovery},
+    magic_endpoint,
+    ticket::NodeTicket,
+    MagicEndpoint,
+};
+
+mod util;
+use tokio::io::{AsyncBufReadExt, BufReader};
+use util::*;
+
+#[derive(Debug, Parser)]
+struct Args {
+    tickets: Vec<NodeTicket>,
+}
+
+/// Handle incoming connections by dispatching them to the right handler.
+async fn handle_connections(endpoint: MagicEndpoint, gossip: Gossip) -> anyhow::Result<()> {
+    while let Some(connecting) = endpoint.accept().await {
+        let gossip = gossip.clone();
+        tokio::spawn(async move {
+            let (_, alpn, connection) = magic_endpoint::accept_conn(connecting).await?;
+            if alpn.as_bytes() == iroh_gossip::net::GOSSIP_ALPN {
+                gossip.handle_connection(connection).await?;
+            }
+            anyhow::Ok(())
+        });
+    }
+    Ok(())
+}
+
+/// Print messages from the gossip stream to stdout.
+async fn print_messages(gossip: Gossip, topic: TopicId) -> anyhow::Result<()> {
+    let mut stream = gossip.subscribe(topic).await?;
+    while let Ok(event) = stream.recv().await {
+        match event {
+            Event::Received(ev) => {
+                let text = String::from_utf8_lossy(&ev.content);
+                tracing::info!("received message: {}", text);
+                println!("message {}", text);
+            }
+            ev => {
+                tracing::info!("event {:?}", ev);
+            }
+        }
+    }
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    tracing_subscriber::fmt::init();
+    let args = Args::parse();
+    let secret_key = get_or_create_secret()?;
+    let _public_key = secret_key.public();
+    let topic = TopicId::from([0u8; 32]);
+    let discovery = Box::new(ConcurrentDiscovery::from_services(vec![
+        Box::new(DnsDiscovery::n0_dns()),
+        Box::new(PkarrPublisher::n0_dns(secret_key.clone())),
+    ]));
+
+    let endpoint = MagicEndpoint::builder()
+        .secret_key(secret_key.clone())
+        .alpns(vec![iroh_gossip::net::GOSSIP_ALPN.to_vec()])
+        .discovery(discovery)
+        .bind(0)
+        .await?;
+    let mut my_addr = endpoint.my_addr().await?;
+    let ticket = NodeTicket::new(my_addr.clone())?;
+    println!("I am {}", my_addr.node_id);
+    println!("Connect to me using {}", ticket);
+    my_addr.apply_options(AddrInfoOptions::Id);
+    let short = NodeTicket::new(my_addr.clone())?;
+    println!("Connect to me using {}", short);
+    wait_for_relay(&endpoint).await?;
+    // add all the info from the tickets to the endpoint
+    let mut ids = Vec::new();
+    for ticket in &args.tickets {
+        let addr = ticket.node_addr();
+        endpoint.add_node_addr(addr.clone())?;
+        ids.push(addr.node_id);
+    }
+    let gossip = Gossip::from_endpoint(
+        endpoint.clone(),
+        iroh_gossip::proto::Config::default(),
+        &my_addr.info,
+    );
+    tokio::spawn(handle_connections(endpoint, gossip.clone()));
+    tokio::spawn(print_messages(gossip.clone(), topic));
+    println!("joining topic {}", topic);
+    gossip.join(topic, ids).await?.await?;
+    println!("joined topic");
+    let mut stdin = BufReader::new(tokio::io::stdin()).lines();
+    while let Some(line) = stdin.next_line().await? {
+        gossip.broadcast(topic, line.into_bytes().into()).await?;
+    }
+    Ok(())
+}
---
+++ pipe/version1/src/main.rs
@@ -0,0 +1,111 @@
+use clap::Parser;
+use iroh_net::{
+    key::{PublicKey, SecretKey},
+    magic_endpoint,
+    ticket::NodeTicket,
+    MagicEndpoint,
+};
+use tracing::info;
+mod util;
+use util::*;
+
+/// The ALPN we use for this protocol.
+const PIPE_ALPN: &[u8] = b"JOTB_PIPE";
+
+#[derive(Debug, clap::Parser)]
+struct Args {
+    /// Ticket to connect to. If not provided, the program will listen for incoming connections.
+    ticket: Option<NodeTicket>,
+}
+
+/// Connect to a remote node using a ticket.
+async fn connect(ticket: NodeTicket) -> anyhow::Result<()> {
+    let secret_key = SecretKey::generate();
+    let public_key = secret_key.public();
+    // Create a new MagicEndpoint with the secret key.
+    // We bind to port 0 to let the OS choose a random port.
+    let endpoint = MagicEndpoint::builder()
+        .secret_key(secret_key)
+        .bind(0)
+        .await?;
+    let addr = ticket.node_addr().clone();
+    info!("connecting to {:?}", addr);
+    let connection = endpoint.connect(addr, PIPE_ALPN).await?;
+    let (mut send, recv) = connection.open_bi().await?;
+    tracing::info!("opened bidirectional stream");
+    tracing::info!("copying from stdin to remote");
+    let remote_node_id = magic_endpoint::get_remote_node_id(&connection)?;
+    let remote = remote_node_id.to_string();
+    send.write_all(format!("hello from {}\n", public_key).as_bytes())
+        .await?;
+    tokio::spawn(copy_to_stdout(remote, recv));
+    copy_stdin_to(send).await?;
+    Ok(())
+}
+
+/// Handle a single incoming connection.
+async fn handle_connecting(my_id: &PublicKey, connecting: quinn::Connecting) -> anyhow::Result<()> {
+    info!("connection attempt");
+    // accept the connection and get the ALPN and the bidirectional stream.
+    let (remote_node_id, alpn, connection) = magic_endpoint::accept_conn(connecting).await?;
+    info!(
+        "got connection from {} using ALPN {:?}",
+        remote_node_id, alpn
+    );
+    // check if the ALPN is what we expect.
+    if alpn.as_bytes() != PIPE_ALPN {
+        tracing::warn!("unexpected ALPN: {:?}", alpn);
+        return Ok(());
+    }
+    // we have already accepted the connection, but we need to accept a stream on the connection.
+    let (mut send, recv) = connection.accept_bi().await?;
+    info!("accepted bidirectional stream");
+    info!("copying from stdin to remote");
+    let author = remote_node_id.to_string();
+    // Send a greeting to the remote node.
+    send.write_all(format!("hello from {}\n", my_id).as_bytes())
+        .await?;
+    // Spawn two tasks to copy data in both directions.
+    tokio::spawn(copy_stdin_to(send));
+    tokio::spawn(copy_to_stdout(author, recv));
+    // this will return immediately, the tasks will keep running in the background.
+    Ok(())
+}
+
+/// Accept incoming connections.
+async fn accept() -> anyhow::Result<()> {
+    let secret_key = get_or_create_secret()?;
+    let public_key = secret_key.public();
+    let endpoint = MagicEndpoint::builder()
+        .secret_key(secret_key)
+        .alpns(vec![PIPE_ALPN.to_vec()])
+        .bind(0)
+        .await?;
+    wait_for_relay(&endpoint).await?;
+    let addr = endpoint.my_addr().await?;
+    println!("I am {}", addr.node_id);
+    println!("Listening on {:#?}", addr.info);
+    println!("ticket: {}", NodeTicket::new(addr)?);
+    while let Some(connecting) = endpoint.accept().await {
+        // handle each incoming connection in separate tasks.        // handle each incoming connection in separate tasks.
+        if let Err(cause) = handle_connecting(&public_key, connecting).await {
+            tracing::warn!("error handling connection: {:?}", cause);
+        }
+    }
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    // init logging. we can now configure the log level with the RUST_LOG environment variable.
+    tracing_subscriber::fmt::init();
+    // Parse the command line arguments.
+    let args = Args::parse();
+    // if a ticket is provided, connect to the remote node, otherwise accept incoming connections.
+    if let Some(ticket) = args.ticket {
+        connect(ticket).await?;
+    } else {
+        accept().await?;
+    }
+    Ok(())
+}
