410fa298c9d14a23a803f0219a43530671503c32
Ruediger Klaehn <rklaehn@protonmail.com>
2024-08-18 15:38:43 +0300

    fix pipe1 example

--- pipe1/src/main.rs
+++ pipe1/src/main.rs
@@ -1,9 +1,9 @@
 use clap::Parser;
 use iroh_net::{
+    endpoint::{self, get_remote_node_id},
     key::{PublicKey, SecretKey},
-    magic_endpoint,
     ticket::NodeTicket,
-    MagicEndpoint,
+    Endpoint,
 };
 use tracing::info;
 mod util;
@@ -24,17 +24,14 @@ async fn connect(ticket: NodeTicket) -> anyhow::Result<()> {
     let public_key = secret_key.public();
     // Create a new MagicEndpoint with the secret key.
     // We bind to port 0 to let the OS choose a random port.
-    let endpoint = MagicEndpoint::builder()
-        .secret_key(secret_key)
-        .bind(0)
-        .await?;
+    let endpoint = Endpoint::builder().secret_key(secret_key).bind(0).await?;
     let addr = ticket.node_addr().clone();
     info!("connecting to {:?}", addr);
     let connection = endpoint.connect(addr, PIPE_ALPN).await?;
     let (mut send, recv) = connection.open_bi().await?;
     tracing::info!("opened bidirectional stream");
     tracing::info!("copying from stdin to remote");
-    let remote_node_id = magic_endpoint::get_remote_node_id(&connection)?;
+    let remote_node_id = endpoint::get_remote_node_id(&connection)?;
     let remote = remote_node_id.to_string();
     send.write_all(format!("hello from {}\n", public_key).as_bytes())
         .await?;
@@ -44,16 +41,21 @@ async fn connect(ticket: NodeTicket) -> anyhow::Result<()> {
 }
 
 /// Handle a single incoming connection.
-async fn handle_connecting(my_id: &PublicKey, connecting: quinn::Connecting) -> anyhow::Result<()> {
+async fn handle_connecting(
+    my_id: &PublicKey,
+    mut connecting: iroh_net::endpoint::Connecting,
+) -> anyhow::Result<()> {
     info!("connection attempt");
     // accept the connection and get the ALPN and the bidirectional stream.
-    let (remote_node_id, alpn, connection) = magic_endpoint::accept_conn(connecting).await?;
+    let alpn: Vec<u8> = connecting.alpn().await?;
+    let connection = connecting.await?;
+    let remote_node_id = get_remote_node_id(&connection)?;
     info!(
         "got connection from {} using ALPN {:?}",
         remote_node_id, alpn
     );
     // check if the ALPN is what we expect.
-    if alpn.as_bytes() != PIPE_ALPN {
+    if alpn.as_slice() != PIPE_ALPN {
         tracing::warn!("unexpected ALPN: {:?}", alpn);
         return Ok(());
     }
@@ -76,13 +78,13 @@ async fn handle_connecting(my_id: &PublicKey, connecting: quinn::Connecting) -> 
 async fn accept() -> anyhow::Result<()> {
     let secret_key = get_or_create_secret()?;
     let public_key = secret_key.public();
-    let endpoint = MagicEndpoint::builder()
+    let endpoint = Endpoint::builder()
         .secret_key(secret_key)
         .alpns(vec![PIPE_ALPN.to_vec()])
         .bind(0)
         .await?;
     wait_for_relay(&endpoint).await?;
-    let addr = endpoint.my_addr().await?;
+    let addr = endpoint.node_addr().await?;
     println!("I am {}", addr.node_id);
     println!("Listening on {:#?}", addr.info);
     println!("ticket: {}", NodeTicket::new(addr)?);
